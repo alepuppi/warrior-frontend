6c47c4cccf6cdf285d943cfab314bd221f83d348
Ale Puppi <guillenn2002@gmail.com>
2025-06-23 12:12:12 -0500

    Corrijo ruta del CSS en MembresiasList.jsx

--- src/components/MembresiasList.jsx
+++ src/components/MembresiasList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./Modal.css";
+import "./MembresiaModal.css"
 
 const MembresiasList = ({ cerrar }) => {
   const [membresias, setMembresias] = useState([]);
